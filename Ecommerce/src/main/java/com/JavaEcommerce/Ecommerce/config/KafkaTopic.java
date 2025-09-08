edd0b924484c149fc9ec7161c8ae8752944a88ca
maheshgadidala <109794652+maheshgadidala@users.noreply.github.com>
2025-09-08 16:54:10 +0530

    SpringSecurity

--- Ecommerce/src/main/java/com/JavaEcommerce/Ecommerce/config/KafkaTopic.java
+++ Ecommerce/src/main/java/com/JavaEcommerce/Ecommerce/config/KafkaTopic.java
@@ -1,23 +1,23 @@
-package com.JavaEcommerce.Ecommerce.config;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class KafkaTopic {
-
-    @Bean
-
-    public NewTopic productTopic(){
-
-        return new NewTopic("product-topic", 5, (short) 1);
-    }
-
-    @Bean
-    public NewTopic productJsonTopic(){
-
-        return new NewTopic("productJson-topic", 5, (short) 1);
-    }
-
-}
+//package com.JavaEcommerce.Ecommerce.config;
+//
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class KafkaTopic {
+//
+//    @Bean
+//
+//    public NewTopic productTopic(){
+//
+//        return new NewTopic("product-topic", 5, (short) 1);
+//    }
+//
+//    @Bean
+//    public NewTopic productJsonTopic(){
+//
+//        return new NewTopic("productJson-topic", 5, (short) 1);
+//    }
+//
+//}
--- Ecommerce/src/main/java/com/JavaEcommerce/Ecommerce/service/KafkaConsumer.java
+++ Ecommerce/src/main/java/com/JavaEcommerce/Ecommerce/service/KafkaConsumer.java
@@ -1,18 +1,18 @@
-package com.JavaEcommerce.Ecommerce.service;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
-
-@Service
-public class KafkaConsumer {
-
-    private static final Logger logger = Logger.getLogger(KafkaConsumer.class.getName());
-    @KafkaListener(topics = "product-topic", groupId = "EcommerceGroup")
-    public void consume(String message) {
-        logger.info(String.format("Consumed message: %s", message));
-
-
-    }
-}
+//package com.JavaEcommerce.Ecommerce.service;
+//
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.logging.Logger;
+//
+//@Service
+//public class KafkaConsumer {
+//
+//    private static final Logger logger = Logger.getLogger(KafkaConsumer.class.getName());
+//    @KafkaListener(topics = "product-topic", groupId = "EcommerceGroup")
+//    public void consume(String message) {
+//        logger.info(String.format("Consumed message: %s", message));
+//
+//
+//    }
+//}
